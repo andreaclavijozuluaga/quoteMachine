9c69178d2cc4d8a8ce29fbb2409d4f5246fff67f
andreaclavijozuluaga <60928580+andreaclavijozuluaga@users.noreply.github.com>
2020-04-11 09:41:20 -0500

    detalles son importantes

--- src/Component/QuoteMachine.js
+++ src/Component/QuoteMachine.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/iconButton';
+import IconButton from '@material-ui/core/IconButton';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
